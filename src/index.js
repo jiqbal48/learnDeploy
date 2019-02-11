caa04086e33f1d9dc62fe5f49f7da2575a26adc0
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-02-11 13:51:47 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import "./styles.css";
+import React from 'react';
+import reactDom from 'react-dom';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+
+reactDom.render(<h1>hello react!</h1>, document.getElementById('app'));
